80192e99436da2c42edff4c6ce8bf67136275c32
gastonnicora <gastonmatias.21@gmail.com>
2026-09-06 18:26:12 -0300

    Remove unused import for ResultActions in DeleteVehicleTest

--- src/test/java/com/gastonnicora/trips/integration/company/vehicle/DeleteVehicleTest.java
+++ src/test/java/com/gastonnicora/trips/integration/company/vehicle/DeleteVehicleTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.gastonnicora.trips.dtos.entities.VehicleDTO;
